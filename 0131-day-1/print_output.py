2563be3a4aee0a5a9893df89ba1465f69531a87d
Michael Del Toro <mdt053123@Michaels-MacBook-Pro-7.local>
2026-01-31 11:05:42 -0500

    first files

---
+++ 0131-day-1/print_output.py
@@ -0,0 +1,12 @@
+print("Hello, World!")
+print("I love pizza\n") # \n ~ new line
+print("ABC")
+print("Sentence 1\nSentence 2")
+print("A\tB") # \t ~ tab (spaces)
+
+print('Single Quotes')
+print('Quote: "quote text"')
+print('''hello "world"
+hello world
+hello world
+hello world''')
---
+++ 0131-day-1/variables.py
@@ -0,0 +1,32 @@
+# TYPE 1: int
+x = 3 # equiv. to x: int = 3
+print(x)
+print(type(x))
+print()
+
+# TYPE 2: float
+y = 3.2
+print(y)
+print(type(y))
+print()
+
+# TYPE 3: bool
+t = True
+f = False
+
+print(t)
+print(f)
+print(type(t))
+print(t*3)
+print()
+
+# TYPE 4: str
+s = "abc"
+print(s)
+print(type(s))
+print()
+
+# TYPE 5: list
+my_list = [3, 4, 7]
+print(my_list)
+print(type(my_list))
