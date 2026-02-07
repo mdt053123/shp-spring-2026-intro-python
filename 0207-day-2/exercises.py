f1b9da66a8b00469791a7182e19300d5ff66602e
Michael Del Toro <mdt053123@Michaels-MacBook-Pro-7.local>
2026-02-07 12:30:37 -0500

    final files

--- 0207-day-2/exercises.py
+++ 0207-day-2/exercises.py
@@ -6,11 +6,25 @@ def divisible_by_3(n):
     return False if not
     """
     
+    return n % 3 == 0
+    
 def sqrt(x):
     """
     Return the sqrt of x.
     DO NOT use x**(1/2)
     """
+    
+    if x < 0:
+        return None  # or raise ValueError
+
+    if x == 0:
+        return 0
+
+    guess = x
+    while abs(guess * guess - x) > 1e-6:
+        guess = (guess + x / guess) / 2
+
+    return guess
     
 def guess_the_number():
     """
@@ -22,6 +36,22 @@ def guess_the_number():
     Hint: Use random.randint(a, b)
     """
     
+    guessed = False
+    n = random.randint(1, 100)
+    
+    while not guessed:
+        guess = int(input("Enter a guess (i.e. 3): "))
+        
+        if guess == n:
+            print("You guessed it!")
+            guessed = True
+        elif guess <= 100 and guess > n:
+            print("Too high!")
+        elif guess >= 1 and guess < n:
+            print("Too low!")
+        else:
+            print("Out of bounds!")
+                    
 print(divisible_by_3(6)) # True
 print(divisible_by_3(2)) # False
 print(sqrt(4)) # 2.0
