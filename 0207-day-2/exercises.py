96e95eb2362303ac60a9d05a17da4a8ae142d1fc
Michael Del Toro <mdt053123@Michaels-MacBook-Pro-7.local>
2026-02-07 12:04:54 -0500

    added exercises.py empty

---
+++ 0207-day-2/exercises.py
@@ -0,0 +1,29 @@
+import random
+
+def divisible_by_3(n):
+    """
+    return True if n is divisible by 3,
+    return False if not
+    """
+    
+def sqrt(x):
+    """
+    Return the sqrt of x.
+    DO NOT use x**(1/2)
+    """
+    
+def guess_the_number():
+    """
+    Generate a random number between 1 and 100,
+    repeatedly ask the user to guess the number;
+    if wrong guess, tell them if higher or lower,
+    if right, print "Success" and end the game
+    
+    Hint: Use random.randint(a, b)
+    """
+    
+print(divisible_by_3(6)) # True
+print(divisible_by_3(2)) # False
+print(sqrt(4)) # 2.0
+print(sqrt(5)) # ~2.236
+guess_the_number() # Play the game
