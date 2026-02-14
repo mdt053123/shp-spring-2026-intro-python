ed17533d7533225b92983ead9d61a96fba60f8ca
Michael Del Toro <mdt053123@Michaels-MacBook-Pro-9.local>
2026-02-14 12:35:13 -0500

    sols

--- 0214-day-3/exercises.py
+++ 0214-day-3/exercises.py
@@ -10,6 +10,12 @@ def first_occurence(nums, num):
     Example 3: nums [1, 0], num = 2 -> return -1
     '''
     
+    for i in range(len(nums)):
+        if nums[i] == num:
+            return i
+        
+    return -1
+    
 def print_reverse(nums):
     '''
     Print each element in nums in reverse order (on one line).
@@ -18,6 +24,9 @@ def print_reverse(nums):
     Example 2: [1, 2, 1, 2] -> prints [2, 1, 2, 1]
     '''
     
+    for i in range(len(nums) - 1, -1, -1):
+        print(nums[i], end=' ')
+    
 def dist_3d(p1, p2):
     '''
     Given p1 = (p1_x, p1_y, p1_z),
@@ -25,6 +34,7 @@ def dist_3d(p1, p2):
     
     Return euclidean distance between p1, p2
     '''
+    return ((p2[0]-p1[0])**2 + (p2[1]-p1[1])**2 + (p2[2]-p1[2])**2)**(1/2)
     
 def value_counts(nums):
     '''
@@ -34,3 +44,19 @@ def value_counts(nums):
     Example: nums = [1, 1, 2, 3, 3, 3, 4, 5, 5],
              prints 1: 2, 2: 1, 3: 3, 4: 1, 5: 2
     '''
+    
+    counts = {}
+    
+    for num in nums:
+        if num not in counts.keys():
+            counts[num] = 1
+        else:
+            counts[num] += 1
+            
+    print(counts)
+    
+print(first_occurence([1, 2, 3, 4, 4, 5], 4))
+print_reverse([1, 2, 3, 4, 4, 5])
+print()
+print(dist_3d((1, 2, 3), (4, 5, 6)))
+value_counts([1, 2, 3, 4, 4, 5])
