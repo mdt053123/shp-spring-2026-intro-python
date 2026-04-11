a57e789ba25d933b11006f49bf8e52c456891d88
Michael Del Toro <mdt053123@Michaels-MacBook-Pro-253.local>
2026-04-11 11:59:32 -0400

    added day8

---
+++ 0411-day-8/review.py
@@ -0,0 +1,14 @@
+my_list = ["apple", 34, True]
+
+for elem in my_list:
+    print(elem)
+    
+my_list.append("banana")
+
+print(my_list)
+
+grades = { "Bob": 85,
+           "Alice": 99,
+           "Joseph": 24 }
+
+print(grades["Alice"])
